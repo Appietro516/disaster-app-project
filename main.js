56698f305b7a5298736cc2a083c09a08b93c9474
Aniket Panda <aniketpanda100@email.arizona.edu>
2023-04-09 00:35:34 -0700

    added dropdown menus

--- main.js
+++ main.js
@@ -1,93 +1,182 @@
-import Map from 'ol/Map.js';
-//import Stamen from 'ol/source/Stamen.js';
-import View from 'ol/View.js';
-import {Layer, Tile as TileLayer} from 'ol/layer.js';
-import {fromLonLat, toLonLat} from 'ol/proj.js';
-import {getCenter, getWidth} from 'ol/extent.js';
-import OSM from 'ol/source/OSM';
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import Feature from 'ol/Feature.js';
+//import OSM from 'ol/source/OSM';
 
-class CanvasLayer extends Layer {
-  constructor(options, dim) {
-    super(options);
+import csv from './emdat_earthquake.csv'
+import VectorLayer from 'ol/layer/Vector.js'
+import MultiPoint from 'ol/geom/MultiPoint.js'
 
-    this.features = options.features;
-    this.dim = options.dim;
+import GeoJSON from 'ol/format/GeoJSON'
+import {Circle, GeometryCollection, Point, Polygon} from 'ol/geom.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
 
-    this.svg = d3
-      .select(document.createElement('div'))
-      .append('svg')
-      .style('position', 'absolute');
+import {
+  Circle as CircleStyle,
+  Fill,
+  Stroke,
+  Style,
+} from 'ol/style.js';
 
-    //this.svg.append('path').datum(this.features).attr('class', 'boundary');
-  }
 
-  getSourceState() {
-    return 'ready';
-  }
+// For D3 integration
+// https://openlayers.org/en/latest/examples/d3.html
 
-  render(frameState) {
-    const width = frameState.size[0];
-    const height = frameState.size[1];
-    const projection = frameState.viewState.projection;
-    const d3Projection = d3.geoMercator().scale(1).translate([0, 0]);
-    let d3Path = d3.geoPath().projection(d3Projection);
 
-    const pixelBounds = d3Path.bounds(this.features);
-    const pixelBoundsWidth = pixelBounds[1][0] - pixelBounds[0][0];
-    const pixelBoundsHeight = pixelBounds[1][1] - pixelBounds[0][1];
+// sample d3 integration
+// class CanvasLayer extends Layer {
+//   constructor(options) {
+//     super(options);
 
-    const geoBounds = d3.geoBounds(this.features);
-    const geoBoundsLeftBottom = fromLonLat(geoBounds[0], projection);
-    const geoBoundsRightTop = fromLonLat(geoBounds[1], projection);
-    let geoBoundsWidth = geoBoundsRightTop[0] - geoBoundsLeftBottom[0];
-    if (geoBoundsWidth < 0) {
-      geoBoundsWidth += getWidth(projection.getExtent());
-    }
-    const geoBoundsHeight = geoBoundsRightTop[1] - geoBoundsLeftBottom[1];
+//     this.features = options.features;
 
-    const widthResolution = geoBoundsWidth / pixelBoundsWidth;
-    const heightResolution = geoBoundsHeight / pixelBoundsHeight;
-    const r = Math.max(widthResolution, heightResolution);
-    const scale = r / frameState.viewState.resolution;
+//     this.svg = d3
+//       .select(document.createElement('div'))
+//       .append('svg')
+//       .style('position', 'absolute');
 
-    const center = toLonLat(getCenter(frameState.extent), projection);
-    const angle = (-frameState.viewState.rotation * 180) / Math.PI;
+//     this.svg.append('path').datum(this.features).attr('class', 'boundary');
+//   }
 
-    d3Projection
-      .scale(scale)
-      .center(center)
-      .translate([width / 2, height / 2])
-      .angle(angle);
+//   getSourceState() {
+//     return 'ready';
+//   }
 
-    d3Path = d3Path.projection(d3Projection);
-    d3Path(this.features);
+//   render(frameState) {
+//     const width = frameState.size[0];
+//     const height = frameState.size[1];
+//     const projection = frameState.viewState.projection;
+//     const d3Projection = d3.geoMercator().scale(1).translate([0, 0]);
+//     let d3Path = d3.geoPath().projection(d3Projection);
 
-    this.svg.attr('width', width);
-    this.svg.attr('height', height);
-    this.svg.append("text")
-    .attr("x", "230")
-    .attr("y", "380")
-    .attr("text-anchor", "middle")
-    .style("font-size", "18px")
-    .text(this.dim);
+//     const pixelBounds = d3Path.bounds(this.features);
+//     const pixelBoundsWidth = pixelBounds[1][0] - pixelBounds[0][0];
+//     const pixelBoundsHeight = pixelBounds[1][1] - pixelBounds[0][1];
 
-    for (let i=0; i < this.features.length; i++) {
-      this.svg
-      .append("circle")
-      .attr("cx", this.features[i]["Total Deaths"])
-      .attr("cy", this.features[i]["Total Deaths"])
-      .attr("r", 3)
-      .append("title").text(this.dim + ": " + this.features[i][this.dim]);
-      //.on("mouseover", (event, dot) => 
-      //  onMouseOver(event, dot, this.svg, i));
-    }
+//     const geoBounds = d3.geoBounds(this.features);
+//     const geoBoundsLeftBottom = fromLonLat(geoBounds[0], projection);
+//     const geoBoundsRightTop = fromLonLat(geoBounds[1], projection);
+//     let geoBoundsWidth = geoBoundsRightTop[0] - geoBoundsLeftBottom[0];
+//     if (geoBoundsWidth < 0) {
+//       geoBoundsWidth += getWidth(projection.getExtent());
+//     }
+//     const geoBoundsHeight = geoBoundsRightTop[1] - geoBoundsLeftBottom[1];
 
-    this.svg.select('path').attr('d', d3Path);
+//     const widthResolution = geoBoundsWidth / pixelBoundsWidth;
+//     const heightResolution = geoBoundsHeight / pixelBoundsHeight;
+//     const r = Math.max(widthResolution, heightResolution);
+//     const scale = r / frameState.viewState.resolution;
 
-    //return null;
-    return this.svg.node();
-  }
-}
+//     const center = toLonLat(getCenter(frameState.extent), projection);
+//     const angle = (-frameState.viewState.rotation * 180) / Math.PI;
+
+//     d3Projection
+//       .scale(scale)
+//       .center(center)
+//       .translate([width / 2, height / 2])
+//       .angle(angle);
+
+//     d3Path = d3Path.projection(d3Projection);
+//     d3Path(this.features);
+
+//     this.svg.attr('width', width);
+//     this.svg.attr('height', height);
+
+//     this.svg.select('path').attr('d', d3Path);
+
+//     return this.svg.node();
+//   }
+// }
+
+// https://openlayers.org/en/latest/examples/side-by-side.html
+
+// const convexHullFill = new Fill({
+//   color: 'rgba(255, 153, 0, 0.4)',
+// });
+// const convexHullStroke = new Stroke({
+//   color: 'rgba(204, 85, 0, 1)',
+//   width: 1.5,
+// });
+// const outerCircleFill = new Fill({
+//   color: 'rgba(255, 153, 102, 0.3)',
+// });
+// const innerCircleFill = new Fill({
+//   color: 'rgba(255, 165, 0, 0.7)',
+// });
+// const textFill = new Fill({
+//   color: '#fff',
+// });
+// const textStroke = new Stroke({
+//   color: 'rgba(0, 0, 0, 0.6)',
+//   width: 3,
+// });
+// const innerCircle = new CircleStyle({
+//   radius: 14,
+//   fill: innerCircleFill,
+// });
+// const outerCircle = new CircleStyle({
+//   radius: 20,
+//   fill: outerCircleFill,
+// });
+
+// const style = new Style({
+//   fill: new Fill({
+//     color: 'rgba(255, 255, 255, 0.2)',
+//   }),
+//   stroke: new Stroke({
+//     color: '#33cc33',
+//     width: 2,
+//   }),
+//   image: new CircleStyle({
+//     radius: 7,
+//     fill: new Fill({
+//       color: '#ffcc33',
+//     }),
+//   }),
+// });
+
+// const vector = new VectorLayer({
+//   source: source,
+//   style: function (feature) {
+//     const geometry = feature.getGeometry();
+//     return geometry.getType() === 'GeometryCollection' ? geodesicStyle : style;
+//   },
+// });
+
+const styles = [
+  /* We are using two different styles for the polygons:
+   *  - The first style is for the polygons themselves.
+   *  - The second style is to draw the vertices of the polygons.
+   *    In a custom `geometry` function the vertices of a polygon are
+   *    returned as `MultiPoint` geometry, which will be used to render
+   *    the style.
+   */
+  new Style({
+    stroke: new Stroke({
+      color: 'blue',
+      width: 3,
+    }),
+    fill: new Fill({
+      color: 'rgba(0, 0, 255, 0.1)',
+    }),
+  }),
+  new Style({
+    image: new CircleStyle({
+      radius: 5,
+      fill: new Fill({
+        color: 'orange',
+      }),
+    }),
+    geometry: function (feature) {
+      // return the coordinates of the first ring of the polygon
+      const coordinates = feature.getGeometry().getCoordinates()[0];
+      return new MultiPoint(coordinates);
+    },
+  }),
+];
+
+
 
 
 const view = new View({
@@ -95,64 +184,208 @@ const view = new View({
   zoom: 2
 })
 
+const layer = new TileLayer({source: new OSM()})
+
+// const map = new Map({
+//   target: 'map1',
+//   layers: [
+//     new TileLayer({
+//       source: new OSM()
+//     })
+//   ],
+//   view: new View({
+//     center: [0, 0],
+//     zoom: 2
+//   })
+// });
+
+console.log(csv[6].Year)
+console.log(csv[6].Latitude)
+//let header = csv[6].split(',');
+
+const geojsonObject = {
+  'type': 'FeatureCollection',
+  'crs': {
+    'type': 'name',
+    'properties': {
+      'name': 'EPSG:3857',
+    },
+  },
+  'features': [
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Polygon',
+        'coordinates': [
+          [
+            [-5e6, 6e6],
+            [-5e6, 8e6],
+            [-3e6, 8e6],
+            [-3e6, 6e6],
+            [-5e6, 6e6],
+          ],
+        ],
+      },
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Polygon',
+        'coordinates': [
+          [
+            [-2e6, 6e6],
+            [-2e6, 8e6],
+            [0, 8e6],
+            [0, 6e6],
+            [-2e6, 6e6],
+          ],
+        ],
+      },
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Polygon',
+        'coordinates': [
+          [
+            [1e6, 6e6],
+            [1e6, 8e6],
+            [3e6, 8e6],
+            [3e6, 6e6],
+            [1e6, 6e6],
+          ],
+        ],
+      },
+    },
+    {
+      'type': 'Feature',
+      'geometry': {
+        'type': 'Polygon',
+        'coordinates': [
+          [
+            [-2e6, -1e6],
+            [-1e6, 1e6],
+            [0, -1e6],
+            [-2e6, -1e6],
+          ],
+        ],
+      },
+    },
+  ],
+};
+let features = [];
+initializeQuadrant(1, csv);
+
+function initializeQuadrant(quadrantNum, quadrantData){
+  //Let's cycle through the JSON data.  
+  
+  for(let i = 0; i < quadrantData.length; i++){
+    let data = quadrantData[i];
+    let longitude = Number(data.Longitude);
+    let latitude = Number(data.Latitude);
+    let point = new Point([longitude, latitude]);
+    let feature = new Feature(point
+        //geometry: new Circle([-122.48, 37.67], 1e6)
+      )
+    features.push(feature);    
+  } 
+  
+}
+
+const source = new VectorSource({
+  features: new GeoJSON().readFeatures(geojsonObject)
+});
+
+const vectorLayer = new VectorLayer({
+  source:source,
+  style: styles
+});
+
+const image = new CircleStyle({
+  radius: 5,
+  fill: null,
+  stroke: new Stroke({color: 'red', width: 1}),
+});
+
+const vectorLayer2 = new VectorLayer({
+  source: new VectorSource({
+    features: features,
+    style: {
+      'circle-radius': 30,
+      'circle-fill-color':"red"
+    }
+  })
+})
+
 const map1 = new Map({
-  layers: [new TileLayer({source: new OSM()})],
   target: 'map1',
+  layers: [new TileLayer({source: new OSM()})],
   view: view,
 });
 
 const map2 = new Map({
-  layers: [new TileLayer({source: new OSM()})],
   target: 'map2',
+  layers: [new TileLayer({source: new OSM()}), vectorLayer2],
   view: view,
 });
 
 const map3 = new Map({
-  layers: [new TileLayer({source: new OSM()})],
   target: 'map3',
+  layers: [new TileLayer({source: new OSM()})],
   view: view,
 });
 
-/**
- * Load the topojson data and create an ol/layer/Image for that data.
- */
-/*
-d3.json('data/topojson/us.json').then(function (us) {
-  const layer = new CanvasLayer({
-    features: topojson.feature(us, us.objects.counties),
-  });
-
-  map.addLayer(layer);
+// Open question: how do we switch off of map views for graph/line charts?
+const map4 = new Map({
+  target: 'map4',
+  layers: [new TileLayer({source: new OSM()})],
+  view: view,
 });
-*/
-let dims = ["Dis Mag Value", "Total Deaths", "Total Damages ('000 US$)"];
-let dummy_data = 
-  [{"Dis Mag Value": 1, "Total Deaths": 100, "Total Damages ('000 US$)": 1000},
-  {"Dis Mag Value": 2, "Total Deaths": 200, "Total Damages ('000 US$)": 2000},
-  {"Dis Mag Value": 3, "Total Deaths": 300, "Total Damages ('000 US$)": 3000}];
 
-const layer1 = new CanvasLayer({
-    features: dummy_data,
-    dim : dims[0]
-  });
-map1.addLayer(layer1);
-
-const layer2 = new CanvasLayer({
-    features: dummy_data,
-    dim : dims[1]
-  });
-map2.addLayer(layer2);
-
-const layer3 = new CanvasLayer({
-    features: dummy_data,
-    dim : dims[2]
-  });
-map3.addLayer(layer3);
-
-/*
-function onMouseOver(event, dot, canvas, i) {
-  //canvas.append("rect")
-  //.attr("")
-  d3.selectAll("circle").append("title").text("popout");
+// event handler for when quadrant dropdown menus change
+function dropDownChange(quadrant) {
+  let dim = document.getElementById("select" + quadrant).value;
+  // quadrant 1-4 specifies which map was changed,
+  // dim specifies which option was selected
+  console.log("Quadrant", quadrant, ":", dim);
 }
-*/
+
+// dims hold the data attributes the user can pick from dropdown menu
+let dims = ["Dis Mag Value", "Total Deaths", "Total Damages ('000 US$)"];
+
+// for each quadrant
+for (let i = 1; i < 5; i++) {
+
+  // add event handler to each menu
+  d3.select("#select" + i)
+  .on("change", function() { dropDownChange(i); });
+
+  // Q2/Q3 option specific to last quadrant
+  if (i == 4) {
+    d3.select("#select" + i)
+    .append("option")
+    .text("Q2/Q3");
+  }
+
+  // add an option for each dim to menu
+  for (let j = 0; j < dims.length; j++) {
+    d3.select("#select" + i)
+    .append("option")
+    .text(dims[j]);
+  }
+
+  // rotate dims
+  let firstElement = dims.shift();
+  dims.push(firstElement);
+}
+
+/**
+ * TODO: Load the jsn data
+ */
+// d3.json('PATH_TO_JSON').then(function (us) {
+//   const layer = new CanvasLayer({
+//     features: topojson.feature(us, us.objects.counties),
+//   });
+
+//   map.addLayer(layer);
+// });
