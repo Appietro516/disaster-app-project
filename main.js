cb6bc8c63f3e0aac8dd091d3de63af5be3078640
Anthony <appietro516@email.arizona.edu>
2023-04-23 15:03:31 -0700

    wire all quadrants, attempt to normalize

--- main.js
+++ main.js
@@ -21,8 +21,11 @@ import {
 
 import $ from "jquery";
 
-//global JsonData
+
+
 let csvData  = csv;
+let features = getFeatures(csvData);
+refreshDropdowns(csvData)
 
 class CanvasLayer extends Layer {
   constructor(options, dim) {
@@ -198,39 +201,47 @@ const styles = [
 ];
 
 
-
-
 const view = new View({
   center: [0, 0],
   zoom: 2
 })
 
-const layer = new TileLayer({source: new OSM()})
 
-// const map = new Map({
-//   target: 'map1',
-//   layers: [
-//     new TileLayer({
-//       source: new OSM()
-//     })
-//   ],
-//   view: new View({
-//     center: [0, 0],
-//     zoom: 2
-//   })
-// });
-
-
-let features = initializeQuadrant(1, csvData);
-
-function initializeQuadrant(quadrantNum, quadrantData, field){
+function getFeatures(quadrantData, field){
   //Let's cycle through the JSON data.  
   //let projection = map2.getView().getProjection();
+  let magnitudes = [];
+  for(let i = 0; i < quadrantData.length; i++){
+    let data = quadrantData[i];
+    console.log(data)
+    let fieldData = data[field || "Dis Mag Value"].replace(/[^0-9.]/g,'');
+    console.log(fieldData)
+    let magnitude = Number(fieldData);
+    magnitudes.push(magnitude)
+  }
+
+  let normalizedMags = [];
+  let maxMag =  Math.max(...magnitudes)
+  let minMag = Math.min(...magnitudes)
+  console.log(normalizedMags)
+  console.log(maxMag)
+  console.log(minMag)
+  magnitudes.forEach((m) => {
+    console.log(m)
+    let normal =  (m - minMag) / (maxMag - minMag)
+    console.log(normal)
+    normalizedMags.push(normal)
+  });
+  
+
   let features = [];
   for(let i = 0; i < quadrantData.length; i++){
     let data = quadrantData[i];
     let longitude = Number(data.Longitude.replace(new RegExp("[A-Za-z]", ""), ""));
-    let magnitude = Number(data[field || "Dis Mag Value"]);
+    let magnitude = normalizedMags[i];
+    // console.log(magnitude)
+    // console.log(field)
+    // console.log(field || $("#select" + quadrantNum).find(":selected").val() || "Dis Mag Value")
     if(isNaN(longitude)){
       let test = 0;   
     }
@@ -238,9 +249,10 @@ function initializeQuadrant(quadrantNum, quadrantData, field){
     let point = [longitude, latitude];
     //let center = transform(fromLonLat([-122.48, 37.67]))
     let center =  [-122.48, 37.67];
+    console.log(magnitude)
     let feature = new Feature(//point
     //1e6
-        {geometry: new Circle(fromLonLat(point, get("EPSG:3857")),10000*magnitude )}
+        {geometry: new Circle(fromLonLat(point, get("EPSG:3857")),(1000000/2)*magnitude )}
       )
     features.push(feature);    
   } 
@@ -248,92 +260,15 @@ function initializeQuadrant(quadrantNum, quadrantData, field){
   return features
 }
 
-
-const geojsonObject = {
-  'type': 'FeatureCollection',
-  'crs': {
-    'type': 'name',
-    'properties': {
-      'name': 'EPSG:3857',
-    },
-  },
-  'features': [
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Polygon',
-        'coordinates': [
-          [
-            [-5e6, 6e6],
-            [-5e6, 8e6],
-            [-3e6, 8e6],
-            [-3e6, 6e6],
-            [-5e6, 6e6],
-          ],
-        ],
-      },
-    },
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Polygon',
-        'coordinates': [
-          [
-            [-2e6, 6e6],
-            [-2e6, 8e6],
-            [0, 8e6],
-            [0, 6e6],
-            [-2e6, 6e6],
-          ],
-        ],
-      },
-    },
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Polygon',
-        'coordinates': [
-          [
-            [1e6, 6e6],
-            [1e6, 8e6],
-            [3e6, 8e6],
-            [3e6, 6e6],
-            [1e6, 6e6],
-          ],
-        ],
-      },
-    },
-    {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Polygon',
-        'coordinates': [
-          [
-            [-2e6, -1e6],
-            [-1e6, 1e6],
-            [0, -1e6],
-            [-2e6, -1e6],
-          ],
-        ],
-      },
-    },
-  ],
-};
-
-const source = new VectorSource({
-  features: new GeoJSON().readFeatures(geojsonObject)
-});
-
-const vectorLayer = new VectorLayer({
-  source:source,
-  style: styles
-});
-
-const image = new CircleStyle({
-  radius: 5,
-  fill: null,
-  stroke: new Stroke({color: 'red', width: 1}),
-});
+const vectorLayer1 = new VectorLayer({
+  source: new VectorSource({
+    features: features,
+    style: {
+      'circle-radius': 30,
+      'circle-fill-color':"red"
+    }
+  })
+})
 
 const vectorLayer2 = new VectorLayer({
   source: new VectorSource({
@@ -345,9 +280,29 @@ const vectorLayer2 = new VectorLayer({
   })
 })
 
+const vectorLayer3 = new VectorLayer({
+  source: new VectorSource({
+    features: features,
+    style: {
+      'circle-radius': 30,
+      'circle-fill-color':"red"
+    }
+  })
+})
+
+const vectorLayer4 = new VectorLayer({
+  source: new VectorSource({
+    features: features,
+    style: {
+      'circle-radius': 30,
+      'circle-fill-color':"red"
+    }
+  })
+})
+
 const map1 = new Map({
   target: 'map1',
-  layers: [new TileLayer({source: new OSM()})],
+  layers: [new TileLayer({source: new OSM()}), vectorLayer1],
   view: view,
 });
 
@@ -359,15 +314,18 @@ const map2 = new Map({
 
 const map3 = new Map({
   target: 'map3',
-  layers: [new TileLayer({source: new OSM()})],
+  layers: [new TileLayer({source: new OSM()}), vectorLayer3],
   view: view,
 });
 
 const map4 = new Map({
   target: 'map4',
-  layers: [new TileLayer({source: new OSM()})],
+  layers: [new TileLayer({source: new OSM()}), vectorLayer4],
   view: view,
 });
+
+const maps = [map1, map2, map3, map4]
+const featureLayers = [vectorLayer1, vectorLayer2, vectorLayer3, vectorLayer4]
 
 
 
@@ -400,54 +358,6 @@ function dropDownChange(quadrant) {
 // dims hold the data attributes the user can pick from dropdown menu
 // let dims = ["Dis Mag Value", "Total Deaths", "Total Damages ('000 US$)"];
 // let disaster = csv
-console.log("HI")
-console.log(csv[0])
-let dims = Object.entries(csv[0]).filter(([_,y]) => y != '' && !isNaN(y)).map(([x,_]) => x)
-
-function getUniqueValues(data, fieldName) {
-  let uniqueValues = new Set();
-  for (let item of data) {
-    uniqueValues.add(item[fieldName]);
-  }
-  return Array.from(uniqueValues);
-}
-
-let opts = getUniqueValues(csv, "Disaster Type")
-console.log(opts)
-
-
-
-// for each quadrant
-for (let i = 1; i < 5; i++) {
-
-  // add event handler to each menu
-  d3.select("#select" + i)
-  .on("change", function(e) { dropDownChange(e, i); });
-
-  // Q2/Q3 option specific to last quadrant
-  if (i == 4) {
-    d3.select("#select" + i)
-    .append("option")
-    .text("Q2/Q3");
-  }
-
-  // add an option for each dim to menu
-  for (let j = 0; j < dims.length; j++) {
-    d3.select("#select" + i)
-    .append("option")
-    .text(dims[j]);
-  }
-
-  for (let j = 0; j < opts.length; j++) {
-    d3.select("#disaster-select" + i)
-    .append("option")
-    .text(opts[j]);
-  }
-
-  // rotate dims
-  let firstElement = dims.shift();
-  dims.push(firstElement);
-}
 
 
 
@@ -464,22 +374,8 @@ for (let i = 1; i < 5; i++) {
 
 //drop down change handler
 
-let dropDownChange = (e, i) => {
-  console.log('woop2')
-  console.log(e.target.value)
-  console.log(i)
-  
-  let features = initializeQuadrant(i, csvData, e.target.value)
-  const mSource = new VectorSource({
-    features: features,
-    style: {
-      'circle-radius': 30,
-      'circle-fill-color':"red"
-    }
-  })
-  let l = map2.getLayers().getArray()[1];
-  l.setSource(mSource)
-
+let dropDownChange = (e, i) => {  
+  refreshMaps(csvData, e.target.value, i)
 }
 
 // File upload handler 
@@ -495,7 +391,21 @@ $("#fileForm").on("change", (e) => {
   reader.onload = function(event) {
     fileContent = event.target.result; // Get the file content as a string
     csvData = d3.csvParse(fileContent)
-    let features = initializeQuadrant(1, csvData);
+    refreshDropdowns(csvData)
+    refreshMaps(csvData)
+  };
+
+  reader.readAsText(f)
+})
+
+//refrash all maps or map i
+const refreshMaps = (data, field = null, i = null) => {
+  const refreshMap = (m, mNumber) => {
+    console.log(mNumber)
+    console.log( $("#select" + (mNumber + 1)).val())
+    let selectedField = field || $("#select" + (mNumber + 1)).val()
+    let features = getFeatures(data, selectedField)
+    console.log(features)
     const mSource = new VectorSource({
       features: features,
       style: {
@@ -503,10 +413,70 @@ $("#fileForm").on("change", (e) => {
         'circle-fill-color':"red"
       }
     })
-    let l = map2.getLayers().getArray()[1];
-    l.setSource(mSource)
+    let l = m.getLayers().getArray()[1];
+    l.setSource(mSource);
+  }
 
-  };
+  if (!i) {
+    maps.forEach((m, mNumber) => {
+      console.log(m, mNumber)
+      refreshMap(m, mNumber)
+  })} else {
+    let m = maps[i - 1];
+    refreshMap(m)
+  }
+}
 
-  reader.readAsText(f)
-})
+function refreshDropdowns(data) {
+  let dims = Object.entries(data[0]).filter(([_,y]) => y != '' && !isNaN(y)).map(([x,_]) => x)
+  let opts = getUniqueValues(data, "Disaster Type")
+
+  // for each quadrant
+  for (let i = 1; i < 5; i++) {
+    let elementID = "#select" + i
+    let disasterElementID = "#disaster-select" + i
+    // add event handler to each menu
+    d3.select(elementID)
+    .on("change", function(e) { dropDownChange(e, i); });
+
+    $(elementID).empty()
+
+    // Q2/Q3 option specific to last quadrant
+    if (i == 4) {
+      d3.select(elementID)
+      .append("option")
+      .text("Q2/Q3");
+    }
+
+    // add an option for each dim to menu
+    for (let j = 0; j < dims.length; j++) {
+      d3.select(elementID)
+      .append("option")
+      .text(dims[j]);
+    }
+
+    for (let j = 0; j < opts.length; j++) {
+      d3.select(disasterElementID)
+      .append("option")
+      .text(opts[j]);
+    }
+
+    // rotate dims
+    let firstElement = dims.shift();
+    dims.push(firstElement);
+
+    //select first option
+    $(elementID)[0].selectedIndex = 0;
+    $(disasterElementID)[0].selectedIndex = 0;
+  }
+}
+
+
+function getUniqueValues(data, fieldName) {
+  let uniqueValues = new Set();
+  for (let item of data) {
+    uniqueValues.add(item[fieldName]);
+  }
+  return Array.from(uniqueValues);
+}
+
